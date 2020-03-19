ff8b5b9d1e2358f0e72ead6127e4fbfaf05c6463
hungrykiller <devanshubilthare821@gmail.com>
2020-03-19 14:21:48 +0530

    Done few chages in the header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
 
 function Header() {
-    <img src="https://images.unsplash.com/photo-1521249664898-864e6c1b6d5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+  return (
+    <header>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+        alt="problem"
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;

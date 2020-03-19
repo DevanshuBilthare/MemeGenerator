210caf53e3204fac33ca1ba6f51fe8cbdc279981
hungrykiller <devanshubilthare821@gmail.com>
2020-03-19 14:22:47 +0530

    the memeGenerator si ready to create funny memes

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,34 +1,76 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
-class MemeGenerator extends React.Component{
-    constructor() {
-        super()
-        this.state= {
-            topText:"",
-            bottomText:"",
-        }
-    }
+class MemeGenerator extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImage:
+        "https://images.unsplash.com/photo-1521249692263-e0659c60326e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      allMemeImgs: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    componentDidMount(event) {
-        const {name, value}= event 
-        this.setState({
-            [name]: value
-        })
-    }
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data;
+        this.setState({ allMemeImgs: memes });
+      });
+  }
 
-    render() {
-        return(
-            <div>
-                <Header />
-                <label>
-                    <input onChange={this.handlechange} type=text name="topText" placeHolder="Top Text" value={this.state.topText}/>
-                    <input onChange={this.handlechange} type=text name="bottomText" placeHolder="Bottom Text" value={this.state.bottomText}/>
-                    
-                </label>
-            </div>
-        )
-    }
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
+    const randMemeImg = this.state.allMemeImgs[randNum].url;
+    this.setState({
+      randomImage: randMemeImg
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <br />
+        <form className="meme-form">
+          <input
+            onChange={this.handleChange}
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+          />
+
+          <input
+            onChange={this.handleChange}
+            name="bottomText"
+            placeholder="Bottom Text"
+            value={this.state.bottomText}
+          />
+
+          <button onClick={this.handleClick}>Gen</button>
+        </form>
+
+        <div className="meme">
+          <img src={this.state.randomImage} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MemeGenerator
+export default MemeGenerator;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import MemeGenerator from './MemeGenerator'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import MemeGenerator from "./MemeGenerator";
+import "./style.css";
 
-ReactDOM.render(<MemeGenerator />, document.getElementById('root'));
+ReactDOM.render(<MemeGenerator />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
